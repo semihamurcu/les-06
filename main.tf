bc18e0f67c3d84a5c13971556b244326cde15cc7
Semih <semihamurcu@gmail.com>
2025-05-29 07:01:29 +0000

    .

--- main.tf
+++ main.tf
@@ -8,3 +8,77 @@ resource "esxi_guest" "LAB6VM" {
     virtual_network = "VM Network"          
   }
 }
+
+#Azure
+
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-ubuntu"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-ubuntu"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "ubuntu-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "ubuntu-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Azure Ubuntu VM
+resource "azurerm_linux_virtual_machine" "azure_vm" {
+  name                = "LAB6VM-Azure"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B2ats_v2"
+  admin_username      = "testuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "testuser"
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  disable_password_authentication = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "24_04-lts"
+    version   = "latest"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,16 @@ variable "location" {
   description = "Azure regio"
   type        = string
 }
+
+
+variable "ssh_key_path" {
+  description = "Pad naar de private SSH key (ED25519). De public key wordt automatisch gezocht via `.pub` extensie."
+  type        = string
+  default     = "~/.ssh/devhostnieuw"
+}
+
+variable "ssh_public_key_path" {
+  description = "Pad naar de public SSH key"
+  type        = string
+  default     = "~/.ssh/devhostnieuw.pub"
+}
